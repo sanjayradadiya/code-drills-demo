161c53bc5722731ca2c025c2962383b5d98039d4
Sanjay Radadiya <sanjay.radadiya91@gmail.com>
2024-11-11 18:34:30 +0530

    feat: add dummy balance sheet table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,52 +1,152 @@
 import * as React from 'react';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
   },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
   },
-];
+}));
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
-const paginationModel = { page: 0, pageSize: 5 };
+const BalanceSheetTable = ({ data }: { data: any }) => {
+  const rows = data.Reports[0].Rows;
+  const headerRow = rows?.filter((row: any) => row.RowType === 'Header');
+  const headers = headerRow[0]?.Cells?.map((item: any, index: number) => ({
+    field: `header-cell-${index}`,
+    headerName: item?.Value,
+  }));
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <StyledTableRow>
+              {headers?.map((header: any) => {
+                return <StyledTableCell>{header.headerName}</StyledTableCell>;
+              })}
+            </StyledTableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row: any) => {
+              if (row.RowType === 'Section' && row.Title) {
+                return (
+                  <>
+                    <StyledTableRow>
+                      {row?.Title ? <StyledTableCell colSpan={3}>{row.Title}</StyledTableCell> : <></>}
+                    </StyledTableRow>
+                    <StyledTableRow>
+                      {row?.Rows?.length ? (
+                        row.Rows.map((nestedRow: any, index: number) => (
+                          <>
+                            <StyledTableRow key={`cell-${index}`}>
+                            {nestedRow.Cells.map((cell: any, index: number) => (
+                              <StyledTableCell colSpan={3} key={`nested-cell-${index}`}>
+                                {cell.Value}
+                              </StyledTableCell>
+                            ))}
+                            </StyledTableRow>
+                          </>
+                        ))
+                      ) : (
+                        <></>
+                      )}
+                    </StyledTableRow>
+                  </>
+                );
+              }
+
+              <></>;
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
+
+// Sample data passed to the component as props
+const data = {
+  Reports: [
+    {
+      ReportID: 'BalanceSheet',
+      ReportName: 'Balance Sheet',
+      ReportType: 'BalanceSheet',
+      ReportTitles: [
+        'Balance Sheet',
+        'Demo Company (AU)',
+        'As at 28 February 2018',
+      ],
+      ReportDate: '23 February 2018',
+      UpdatedDateUTC: '/Date(1519358515899)/',
+      Rows: [
+        {
+          RowType: 'Header',
+          Cells: [
+            { Value: '' },
+            { Value: '28 Feb 2018' },
+            { Value: '28 Feb 2017' },
+          ],
+        },
+        {
+          RowType: 'Section',
+          Title: 'Assets',
+        },
+        {
+          RowType: 'Section',
+          Title: 'Bank',
+          Rows: [
+            {
+              RowType: 'Row',
+              Cells: [
+                { Value: 'Business Bank Account' },
+                { Value: '-2894.08' },
+                { Value: '0.00' },
+              ],
+            },
+            {
+              RowType: 'Row',
+              Cells: [
+                { Value: 'Business Savings Account' },
+                { Value: '6878.28' },
+                { Value: '0.00' },
+              ],
+            },
+            {
+              RowType: 'SummaryRow',
+              Cells: [
+                { Value: 'Total Bank' },
+                { Value: '3984.20' },
+                { Value: '0.00' },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
 export default function App() {
-  return (
-    <Paper sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{ pagination: { paginationModel } }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-        sx={{ border: 0 }}
-      />
-    </Paper>
-  );
+  return <BalanceSheetTable data={data} />;
 }
